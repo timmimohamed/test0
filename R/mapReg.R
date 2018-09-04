656ce6581521aafe0698dbc121fe59a649538fbc
timmimohamed <42908023+timmimohamed@users.noreply.github.com>
2018-09-04 12:51:27 +0200

    Update mapReg.R

--- R/mapReg.R
+++ R/mapReg.R
@@ -3,6 +3,6 @@ library(sp)
 library(rgdal)
 mapReg <- function() {
   print("Azul, Morocco!")
-  shapeData <- spTransform(readOGR(dsn="." ,layer = 'regions'), CRS("+proj=longlat +ellps=GRS80"))
+  shapeData <- spTransform(readOGR(dsn="." ,layer = 'https://github.com/timmimohamed/test0/regions'), CRS("+proj=longlat +ellps=GRS80"))
   plot(shapeData)
 }
